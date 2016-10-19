e23d31f0eab22b0f97e4c7dbcfd81a3fa156f752
Cherry Kim <cherry.seniorqa@gmail.com>
2016-10-19 14:18:12 +0700

    change database

--- src/testcase/CheckCSVUrls/UrlsCheckingMaster.java
+++ src/testcase/CheckCSVUrls/UrlsCheckingMaster.java
@@ -28,7 +28,8 @@ public class UrlsCheckingMaster extends AbstractTest {
 		log.info("Pre-condition - 03: Choose available MySQL database and get an Username");
 		log.info("Pre-condition - 04: Click on Config button and get default Password");
 		log.info("Pre-condition - 05: Open the site https://cherry.epmxweb.com");
-		configPage.selectBranchAndDatabase("master", "epmxdata_nydoh_daily_18Oct16");
+//		configPage.selectBranchAndDatabase("master", "epmxdata_nydoh_daily_18Oct16");
+		configPage.selectBranchAndDatabase("master", "epmxdata_csc_daily_16Oct16");
 	}
 	
 	@Test(groups = { "regression" }, description = "Check page is displayed correctly")
